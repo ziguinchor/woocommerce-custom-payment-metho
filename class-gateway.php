212c95e099a61cd8e84d0645fb19bdac72e53a73
Mohammed ezzouhri <ezzouhrivm@gmail.com>
2024-01-22 17:25:43 +0100

    Add Woocommerce Settings Fields

--- class-gateway.php
+++ class-gateway.php
@@ -9,6 +9,15 @@ class Global_Checkout_Gateway extends WC_Payment_Gateway
     $this->method_title = __('Global Checkout', 'global-checkout-gateway');
     $this->method_description = __('Accept payments through Global Checkout', 'global-checkout-gateway');
 
+
+    $this->has_fields = false;
+    $this->method_title = __('Global Checkout', 'global-checkout-gateway');
+    $this->method_description = __('Accept payments through Global Checkout', 'global-checkout-gateway');
+
+    $this->title = $this->get_option('title');
+    $this->description = $this->get_option('description');
+    $this->instructions = $this->get_option('instructions', $this->description);
+
     // Other initialization code goes here
 
     $this->init_form_fields();
@@ -19,14 +28,43 @@ class Global_Checkout_Gateway extends WC_Payment_Gateway
 
   public function init_form_fields()
   {
-    $this->form_fields = array(
-      'enabled' => array(
-        'title' => __('Enable/Disable', 'global-checkout-gateway'),
-        'type' => 'checkbox',
-        'label' => __('Enable Global Checkout Gateway', 'global-checkout-gateway'),
-        'default' => 'yes',
-      ),
-      // Add more settings fields as needed
+    $this->form_fields = apply_filters(
+      'global_checkout_fields',
+      array(
+        'enabled' => array(
+          'title' => __('Enable/Disable', 'global-checkout-gateway'),
+          'type' => 'checkbox',
+          'label' => __('Enable or Disable Global Checkout', 'global-checkout-gateway'),
+          'default' => 'no'
+        ),
+        'title' => array(
+          'title' => __('Global Checkout Gateway', 'global-checkout-gateway'),
+          'type' => 'text',
+          'default' => __('Global Checkout Gateway', 'global-checkout-gateway'),
+          'desc_tip' => true,
+          'description' => __('Add a new title for the Global Checkout Gateway that customers will see when they are in the checkout page.', 'global-checkout-gateway')
+        ),
+        'description' => array(
+          'title' => __('Global Checkout Gateway Description', 'global-checkout-gateway'),
+          'type' => 'textarea',
+          'default' => __('Please remit your payment to the shop to allow for the delivery to be made', 'global-checkout-gateway'),
+          'desc_tip' => true,
+          'description' => __('Add a new title for the Global Checkout Gateway that customers will see when they are in the checkout page.', 'global-checkout-gateway')
+        ),
+        'instructions' => array(
+          'title' => __('Instructions', 'global-checkout-gateway'),
+          'type' => 'textarea',
+          'default' => __('Default instructions', 'global-checkout-gateway'),
+          'desc_tip' => true,
+          'description' => __('Instructions that will be added to the thank you page and odrer email', 'global-checkout-gateway')
+        ),
+        // 'enable_for_virtual' => array(
+        //     'title'   => __( 'Accept for virtual orders', 'woocommerce' ),
+        //     'label'   => __( 'Accept COD if the order is virtual', 'woocommerce' ),
+        //     'type'    => 'checkbox',
+        //     'default' => 'yes',
+        // ),
+      )
     );
   }
 
